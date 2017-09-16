6511e9aec979fb517a550e7fe026f5e424a2ab60
unknown <sreenath@medas-dev-21.medas.dxb>
2017-09-16 12:18:10 +0400

    till vid 7

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const moment = require('moment');
-var now = moment();
-var future = moment().add(10,'months');
-console.log(now.format('DD-MM-YYYY'));
-console.log(future.format('DD-MM-YYYY'));
-console.log("Branch 1");
+const request = require('request');
+const yargs = require('yargs');
+
+const arguments = yargs
+    .options({
+        a:{
+            demand: true,
+            alias: 'address',
+            string: true,
+            describe: 'Arguments to fetch'
+        }    
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+var location = encodeURIComponent(arguments.a);
+
+request({url:`https://maps.googleapis.com/maps/api/geocode/json?address=${location}`, json:true},
+    (error, response, body)=>{
+        let address = body.results[0].formatted_address;
+        let latitude = body.results[0].geometry.location.lat;
+        let longtitude = body.results[0].geometry.location.lng;
+        console.log(`True Address=${address} Lattitude=${latitude} Longtitude=${longtitude}`);
+        // console.log(JSON.stringify(body,undefined,2));
+    });
+
+moment().locale('es');
+console.log(moment().format('DD-MMMM-YYYY'));
